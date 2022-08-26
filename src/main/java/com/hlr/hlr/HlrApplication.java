ae48c99cc9f0fe66731de426400e76f523770324
Mohamad Dhaini <mohamad.dhaini02@lau.edu>
2022-08-26 14:00:45 +0300

    First Commit

---
+++ src/main/java/com/hlr/hlr/HlrApplication.java
@@ -0,0 +1,13 @@
+package com.hlr.hlr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hlr/hlr/HlrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlr.hlr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
